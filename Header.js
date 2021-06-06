31e09f2db6a3d523077be2befed1d56843b88ae8
rutu-web <68337176+rutu-web@users.noreply.github.com>
2021-06-06 16:54:02 +0530

    Add files via upload

---
+++ Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  
+  return(
+    <header>
+    <h1>Javascript and React.JS Bootcamp</h1>
+    </header>
+  );
+}
+
+export default Header;
---
+++ Note.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+  return(
+    <div className="note">
+      <h1>Javascript and React.Js</h1>
+      <p>
+        This was an amazing bootcamp taken by Shaurya Sir.
+        We covered everything from scratchincluding javascript, React.JS and HTML.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
